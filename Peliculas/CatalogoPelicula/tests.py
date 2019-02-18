de47dfb3785c34db48c7e1d23cd75a155b0686e2
Ricardo VB <ricardovb@MacBook-Pro-de-Ricardo.local>
2019-02-18 08:32:48 -0600

    Se modifico pruebas y vistas para el uso de autopep8 y flake8

--- Peliculas/CatalogoPelicula/tests.py
+++ Peliculas/CatalogoPelicula/tests.py
@@ -2,8 +2,9 @@ from django.test import TestCase
 
 # Create your tests here.
 
-class TestCatalogo(TestCase):
-	"""docstring for TestCatalogo"""
-	def test_smoker(self):
-		self.assertEquals(2,1+1)
 
+class TestCatalogo(TestCase):
+    """docstring for TestCatalogo"""
+
+    def test_smoker(self):
+        self.assertEquals(2, 1+1)
--- Peliculas/CatalogoPelicula/views.py
+++ Peliculas/CatalogoPelicula/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def vista(self):
+	if True:
+		pass
+	else:
+		pass
